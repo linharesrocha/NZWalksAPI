b32b464e5708ed489c25e1d8b3b44338c34b2dc8
linharesrocha <guilhermelinharesr@gmail.com>
2024-01-24 19:55:22 -0300

    24. Create New Controller (Regions Controller)

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AUK",
+                    RegionImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Auckland_in_New_Zealand.svg/1200px-Auckland_in_New_Zealand.svg.png",
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellignton Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Auckland_in_New_Zealand.svg/1200px-Auckland_in_New_Zealand.svg.png",
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
