2b5b8e6138d63daf3ef17e986eb4271feb165599
linharesrocha <guilhermelinharesr@gmail.com>
2024-01-25 09:47:27 -0300

    28. Change Methods To Use DTOs

--- Controllers/RegionsController.cs
+++ Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NZWalksAPI.Data;
 using NZWalksAPI.Models.Domain;
+using NZWalksAPI.Models.DTO;
 
 namespace NZWalksAPI.Controllers
 {
@@ -19,21 +20,42 @@ namespace NZWalksAPI.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = dbContext.Regions.ToList();
-            return Ok(regions);
+            var regionsDomain = dbContext.Regions.ToList();
+            var regionsDto = new List<RegionDTO>();
+            foreach(var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDTO()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+
+
+            return Ok(regionsDto);
         }
 
         [HttpGet]
         [Route("{id:Guid}")]
         public IActionResult GetById([FromRoute] Guid id)
         {
-            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
-            if (region == null)
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (regionDomain == null)
             {
                 return NotFound();
             }
 
-            return Ok(region);
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            return Ok(regionDto);
         }
     }
 }
--- Models/DTO/RegionDTO.cs
+++ Models/DTO/RegionDTO.cs
@@ -1,9 +1,9 @@
-﻿namespace NZWalksAPI.Models.Domain
+﻿namespace NZWalksAPI.Models.DTO
 {
-    public class Region
+    public class RegionDTO
     {
         public Guid Id { get; set; }
-        public string Code{ get; set; }
+        public string Code { get; set; }
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
 
