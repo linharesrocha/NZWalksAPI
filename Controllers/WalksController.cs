e3f3e02d6223b0e38c2b46d925c4dabb7cdd9d6a
linharesrocha <guilhermelinharesr@gmail.com>
2024-01-27 13:49:10 -0300

    52. Filtering

--- Controllers/WalksController.cs
+++ Controllers/WalksController.cs
@@ -35,9 +35,9 @@ namespace NZWalksAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
         {
-            var walksDomainModel = await walkRepository.GetAllAsync();
+            var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery);
 
             var walkDto = mapper.Map<List<WalkDto>>(walksDomainModel);
 
--- Repositories/IWalkRepository.cs
+++ Repositories/IWalkRepository.cs
@@ -5,7 +5,7 @@ namespace NZWalksAPI.Repositories
     public interface IWalkRepository
     {
         Task<Walk> CreateAsync(Walk walk);
-        Task<List<Walk>>GetAllAsync();
+        Task<List<Walk>>GetAllAsync(string? filterOn = null, string? filterQuery = null);
 
         Task<Walk?>GetByIdAsync(Guid id);
 
